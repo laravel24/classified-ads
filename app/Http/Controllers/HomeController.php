545b8604b791df548efb627ea367c2662cf2f92d
Ciprian <ciprian88m@gmail.com>
2017-02-14 07:46:18 +0200

    Area selection

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,25 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+use App\Area;
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
+class HomeController extends Controller {
+
+  public function index() {
+    $areas = Area::get()->toTree();
+    return view('home', compact('areas'));
+  }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,40 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <div class="row">
+        @foreach($areas as $country)
+          <div class="col-md-12">
+            <h3>
+              <a href="">
+                {{ $country->name }}
+              </a>
+            </h3>
+            <hr>
+            <div class="row">
+              @foreach($country->children as $state)
+                <div class="col-md-4">
+                  <h4>
+                    <a href="">
+                      {{ $state->name }}
+                    </a>
+                  </h4>
+                  <hr>
+                  @foreach($state->children as $city)
+                    <h5>
+                      <a href="">
+                        {{ $city->name }}
+                      </a>
+                    </h5>
+                  @endforeach
+                </div> <!-- /.col-md-4 -->
+              @endforeach
+            </div> <!-- /.row -->
+          </div> <!-- /.col-md-12 -->
+        @endforeach
+      </div> <!-- /.row -->
+    </div> <!-- /.panel-body -->
+  </div> <!-- /.panel -->
+</div> <!-- /.container -->
 @endsection
