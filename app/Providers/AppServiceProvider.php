e9a8b94c334dd0a49224ac771652fdea1b542793
Ciprian <ciprian88m@gmail.com>
2017-02-16 05:48:30 +0200

    Populating categories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,11 @@ class AppServiceProvider extends ServiceProvider
           $prefix = $area->parent ? $area->parent->name . ' ' : '';
           $area->slug = str_slug($prefix . $area->name);
         });
+
+        \App\Category::creating(function($category) {
+          $prefix = $category->parent ? $category->parent->name . ' ' : '';
+          $category->slug = str_slug($prefix . $category->name);
+        });
     }
 
     /**
