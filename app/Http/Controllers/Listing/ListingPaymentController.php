cf493597c5245eb6b436872f3e0646b5177f8ff5
Ciprian <ciprian88m@gmail.com>
2017-02-21 20:05:16 +0200

    Processing the payment

--- app/Http/Controllers/Listing/ListingPaymentController.php
+++ app/Http/Controllers/Listing/ListingPaymentController.php
@@ -24,9 +24,38 @@ class ListingPaymentController extends Controller {
 
   public function store(Request $request, Area $area, Listing $listing) {
     $this->authorize('touch', $listing);
+
     if($listing->live()) {
       return back();
     }
+
+    if($listing->cost() === 0) {
+      return back();
+    }
+
+    if(($nonce = $request->payment_method_nonce) === null) {
+      return back();
+    }
+
+    $result = \Braintree_Transaction::sale([
+      'amount' => $listing->cost(),
+      'paymentMethodNonce' => $nonce,
+      'options' => [
+        'submitForSettlement' => true
+      ]
+    ]);
+
+    if($result->success === false) {
+      return back()->withError('Something went wrong while processing your payment');
+    }
+
+    $listing->live = true;
+    $listing->created_at = \Carbon\Carbon::now();
+    $listing->save();
+
+    return redirect()
+      ->route('listings.show', [$area, $listing])
+      ->withSuccess('Congratulations! Payment accepted and your listing is live');
   }
 
 }
